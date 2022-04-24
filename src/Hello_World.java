8981e18dcd4a06f54587528abd35af1ded8aba82
Rebeca Garcia <rebecagarciap@hotmail.com>
2022-04-23 21:45:36 -0700

    commit_1

---
+++ src/Hello_World.java
@@ -0,0 +1,10 @@
+public class Hello_World
+{
+    // Main method
+    public static void main(String[] args)
+    {
+
+        // Display output
+        System.out.println("Hello World");
+    }
+}
